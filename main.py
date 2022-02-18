ec68ab01650aa3e70178d18d3dc6c8edc2f8b5ee
Peter Pham <ptrpham2468@gmail.com>
2022-02-17 22:53:29 -0600

    added comments

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
+#############################################################################################################
+# Author:              Peter Pham (pxp180041)
+# Date Started:        02/17/2022
+# IDE:                 pycharm
+#
+# Description:
+# This program is a program specifically tailored for the PDF powerpoints made by Prof Paulk. It trims the
+# PDFs and consolidates it all into one powerpoint. It is currently a set trim amount because trimming
+# dynamically is not within my skill level. This program will consolidate all PDF within the file so be warned.
+#
+# TODO: make a user interface for better control of what the program will do. Option to select trim or no trim
+#############################################################################################################
 import glob
 import os
 
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 
-basePath = r"".join(os.getcwd())
+basePath = r"".join(os.getcwd())  # get the current directory
 files_list = []
 
-filePaths = glob.glob(os.path.join(basePath, '*.pdf'.format('')))
-
-print(filePaths)
+pdfFiles = glob.glob(os.path.join(basePath, '*.pdf'.format('')))  # returns a list of all the PDF files in the directory
 
 writer = PdfFileWriter()  # create a writer to save the updated results
-merger = PdfFileMerger()
 
-for file in filePaths:
+# creates a list of pdf file object types
+for file in pdfFiles:
     pdf = PdfFileReader(file)
 
     files_list.append(pdf)
 
+# open the resulting file to be printed
 with open("result.pdf", "ab+") as f:
     for file in files_list:
-        i = 1
+        # the for loop removes the title page and the last questions page
         for i in range(1, file.getNumPages() - 1):
-
             page = file.getPage(i)
 
+            # trims the page
             page.cropBox.setLowerLeft((60, 60))
             page.cropBox.setUpperRight((561.6, 725))
 
+            # add it to the page to be printed
             writer.addPage(page)
 
+    # write it to file.
     writer.write(f)
-
-
